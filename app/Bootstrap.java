76266ea16ef3093c2068bb74694837df55331ab7
Matthieu Guillermin <matthieu.guillermin@gmail.com>
2010-10-04 18:37:09 +0200

    Home page

---
+++ app/Bootstrap.java
@@ -0,0 +1,14 @@
+import play.*;
+import play.jobs.*;
+import play.test.*;
+
+import models.*;
+
+@OnApplicationStart
+public class Bootstrap extends Job {
+	public void doJob() {
+		if (User.count() == 0) {
+			Fixtures.load("initial-data.yml");
+		}
+	}
+}
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,23 @@
 package controllers;
 
+import java.util.List;
+
+import models.Post;
+import play.Play;
 import play.mvc.*;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+    	Post frontPost = Post.find("order by postedAt desc").first();
+    	List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+        render(frontPost, olderPosts);
+    }
+    
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
     }
 
 }
