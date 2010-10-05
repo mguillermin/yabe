59c195a579b7c4dcc3c3838014e8370b39caab26
Matthieu Guillermin <matthieu.guillermin@gmail.com>
2010-10-05 18:04:43 +0200

    Post comments

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import models.Post;
 import play.Play;
+import play.data.validation.Required;
 import play.mvc.*;
 
 public class Application extends Controller {
@@ -12,6 +13,21 @@ public class Application extends Controller {
     	Post frontPost = Post.find("order by postedAt desc").first();
     	List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
         render(frontPost, olderPosts);
+    }
+    
+    public static void show(Long id) {
+    	Post post = Post.findById(id);
+    	render(post);
+    }
+    
+    public static void postComment(Long postId, @Required String author, @Required String content) {
+    	Post post = Post.findById(postId);
+    	if (validation.hasErrors()) {
+    		render("Application/show.html", post);
+    	}
+    	post.addComment(author, content);
+    	flash.success("Thanks for posting %s", author);
+    	show(postId);
     }
     
     @Before
